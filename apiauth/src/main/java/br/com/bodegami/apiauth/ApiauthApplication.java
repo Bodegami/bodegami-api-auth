4c817f6072341ef885dfac755adeb0055f919bb4
Renato <rena.f.oliveira@gmail.com>
2024-03-29 21:37:45 -0300

    setup inicial

---
+++ apiauth/src/main/java/br/com/bodegami/apiauth/ApiauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.bodegami.apiauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiauthApplication.class, args);
+	}
+
+}
---
+++ apiauth/src/test/java/br/com/bodegami/apiauth/ApiauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bodegami.apiauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
