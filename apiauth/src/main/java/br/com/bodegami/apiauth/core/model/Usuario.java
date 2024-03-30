3c60d7d9ce16a651f980ee4efd303e0617fe49cb
Renato <rena.f.oliveira@gmail.com>
2024-03-29 22:16:37 -0300

    adicinado configs database && adicionado entidade usuario

---
+++ apiauth/src/main/java/br/com/bodegami/apiauth/core/model/Usuario.java
@@ -0,0 +1,24 @@
+package br.com.bodegami.apiauth.core.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+@Entity(name = "Usuario")
+@Table(name = "usuarios")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String login;
+
+    private String senha;
+}
